fbe5e299d3303227535850d15b473f10a8259b63
Anto <leuhzebre@gmail.com>
2019-07-19 22:58:56 -0400

    Added Ship + xOffset control

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 20f;
+    [Tooltip("In m")] [SerializeField] float xRange = 7.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xSpeed * xThrow * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        print(xRange);
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
